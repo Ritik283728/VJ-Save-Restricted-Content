46b2f5bbc72f78a25f23b38fcfbbf4d0d819c6b0
Ritik283728 <155610005+Ritik283728@users.noreply.github.com>
2024-03-14 22:12:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,9 +88,9 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
 			bot.send_message(message.chat.id,"**Chat Joined**", reply_to_message_id=message.id)
 		except UserAlreadyParticipant:
 			bot.send_message(message.chat.id,"**Chat alredy Joined**", reply_to_message_id=message.id)
-		except InviteHashExpired:
-			bot.send_message(message.chat.id,"**Invalid Link**", reply_to_message_id=message.id)
-
+#		except InviteHashExpired:
+#			bot.send_message(message.chat.id,"**Invalid Link**", reply_to_message_id=message.id)
+#
 	# getting message
 	elif "https://t.me/" in message.text:
 
